d682cb112e9b18233e0159ab5af7b578e4f9b0ff
Kevin <101434732+kevinhuytran1128@users.noreply.github.com>
2022-06-15 18:47:05 -0400

    update

--- JS/nav.js
+++ JS/nav.js
@@ -1,9 +1,10 @@
+
 const navSlide = () => {
     document.addEventListener("DOMContentLoaded", () => {
         const burger = document.querySelector('.burger');
         const nav = document.querySelector('.nav-links');
         const navLinks = document.querySelectorAll('.nav-links li');
-    
+        console.log(document.querySelector('.parallax'));
         burger.addEventListener('click', () => {
             nav.classList.toggle('nav-active');
             navLinks.forEach((link, index) => {
