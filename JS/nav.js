041456b86fa377d791f0de61023bd9ca95c96af1
Kevin <101434732+kevinhuytran1128@users.noreply.github.com>
2022-06-15 16:01:48 -0400

    update

--- JS/nav.js
+++ JS/nav.js
@@ -1,8 +1,11 @@
+console.log('bitch');
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-
+    document.addEventListener("DOMContentLoaded", () => {
+        const burger = document.querySelector('.burger');
+        const nav = document.querySelector('.nav-links');
+        const navLinks = document.querySelectorAll('.nav-links li');
+    });
+    console.log(burger);
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
         navLinks.forEach((link, index) => {
